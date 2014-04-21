9e23e1ed70c3c6022926bc8a09da861921091051
Diego Mesa <damesa@eng.ucsd.edu>
2014-04-21 15:13:23 -0700

    Initial commit

---
+++ simplescript.py
@@ -0,0 +1,10 @@
+"""
+simple script
+"""
+# import sys
+# print "script input arguments: ", sys.argv
+# Don’t implement option parsing - use: optparse, argparse, docopt.
+
+message = "Hello how are you?"
+for word in message.split():
+    print word
